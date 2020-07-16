343050fa44d8ce0a66d6662cf7d72f1f2e05af94
David <68331872+David-Matos-Correia@users.noreply.github.com>
2020-07-16 09:45:36 -0300

    Update bot_discord.py

--- bot_discord.py
+++ bot_discord.py
@@ -1,7 +1,10 @@
 import discord
+#required to the library asyncio
 import asyncio
 
 #Your bot's token
+#create a .txt file with your bot's token, and place it together where the .py file is
+#the token is purchased from the Discord developer portal
 def read_token():
     with open("token.txt", "r") as f:
         lines = f.readlines()
@@ -12,10 +15,11 @@ token = read_token()
 
 client = discord.Client()
 
-#Event that will occur
+#Event that will occur when the user writes in the chat
 @client.event
 async def on_message(message):
-    if message.content.find("Pergunta") != -1:
-        await message.channel.send("Resposta")
+    if message.content.find("Question") != -1:
+        #bot response
+        await message.channel.send("Answer")
      
 client.run(token)
